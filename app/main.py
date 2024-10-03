8e5bf1953d20d7dadabf93a2f6443560ec27b2a3
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2024-10-03 21:46:56 +0100

    Set up advice page

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import httpx
+import json
 from datetime import datetime
 
 from fastapi import FastAPI, Request
@@ -5,8 +7,24 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from contextlib import asynccontextmanager
+from starlette.config import Config
+
+# Load environment variables from .env
+config = Config(".env")
+
+
+# Create an async context manager to handle the lifespan of the app
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.requests_client = httpx.AsyncClient()
+    yield
+    await app.requests_client.aclose()
+
+
 # create app instance
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+
 
 # set location for templates
 templates = Jinja2Templates(directory="app/view_templates")
@@ -23,8 +41,15 @@ async def index(request: Request):
 
 @app.get("/advice", response_class=HTMLResponse)
 async def advice(request: Request):
-    """Return the advice.html page."""
-    return templates.TemplateResponse(request=request, name="advice.html")
+    """Return the advice.html page. Gets data from external advice API."""
+
+    requests_client = request.app.requests_client
+
+    response = await requests_client.get(config("ADVICE_URL"))
+
+    return templates.TemplateResponse(
+        "advice.html", {"request": request, "data": response.json()}
+    )
 
 
 @app.get("/apod", response_class=HTMLResponse)
