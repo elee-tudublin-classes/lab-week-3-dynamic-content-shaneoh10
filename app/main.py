b7db7059dbc3abca70a98643f2c0ab515fb1b798
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2024-10-03 21:23:08 +0100

    Set up dynamic server/browser times

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-# import dependencies
+from datetime import datetime
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -10,25 +11,33 @@ app = FastAPI()
 # set location for templates
 templates = Jinja2Templates(directory="app/view_templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     """Return the index.html page."""
-    return templates.TemplateResponse(request=request, name="index.html")
+    server_time: datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "server_time": server_time}
+    )
+
 
 @app.get("/advice", response_class=HTMLResponse)
 async def advice(request: Request):
     """Return the advice.html page."""
     return templates.TemplateResponse(request=request, name="advice.html")
 
+
 @app.get("/apod", response_class=HTMLResponse)
 async def apod(request: Request):
     """Return the apod.html page."""
     return templates.TemplateResponse(request=request, name="apod.html")
 
+
 @app.get("/params", response_class=HTMLResponse)
 async def params(request: Request):
     """Return the params.html page."""
     return templates.TemplateResponse(request=request, name="params.html")
+
 
 app.mount(
     "/static",
